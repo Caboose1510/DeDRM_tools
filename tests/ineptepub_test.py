ea282039033a92f9fd61a62bd72c6ed46f92f379
Laurent Almeras <lalmeras@gmail.com>
2020-05-28 21:24:51 +0200

    decryptor test

--- tests/ineptepub_test.py
+++ tests/ineptepub_test.py
@@ -111,7 +111,7 @@ def test_load_crypto_fallback():
 
 
 def test_RSA_init():
-    from mock import patch, MagicMock, PropertyMock, ANY
+    from mock import patch, MagicMock, PropertyMock
     with patch('ctypes.CDLL') as loader_mock, \
             patch('ctypes.POINTER', side_effect=lambda a: a), \
             patch('ctypes.cast', side_effect=lambda a, b: a), \
@@ -133,6 +133,92 @@ def test_RSA_init():
         restype_mock.assert_called()
         argtypes_mock.assert_called()
         d2i_RSAPrivateKey_mock.assert_called_once_with(None, der, len(der))
+
+
+ENCRYPTION_XML = """<?xml version="1.0"?>
+<encryption xmlns="urn:oasis:names:tc:opendocument:xmlns:container">¬
+  <EncryptedData xmlns="http://www.w3.org/2001/04/xmlenc#">¬
+    <EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#aes128-cbc"></EncryptionMethod>¬
+    <KeyInfo xmlns="http://www.w3.org/2000/09/xmldsig#">¬
+      <resource xmlns="http://ns.adobe.com/adept">urn:uuid:03ba87d8-0c62-43e5-803b-7bdbaf391a3c</resource>¬
+    </KeyInfo>¬
+    <CipherData>¬
+      <CipherReference URI="path1"></CipherReference>¬
+    </CipherData>¬
+  </EncryptedData>¬
+  <EncryptedData xmlns="http://www.w3.org/2001/04/xmlenc#">¬
+    <EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#aes128-cbc"></EncryptionMethod>¬
+    <KeyInfo xmlns="http://www.w3.org/2000/09/xmldsig#">¬
+      <resource xmlns="http://ns.adobe.com/adept">urn:uuid:03ba87d8-0c62-43e5-803b-7bdbaf391a3c</resource>¬
+    </KeyInfo>¬
+    <CipherData>¬
+      <CipherReference URI="path2"></CipherReference>¬
+    </CipherData>¬
+  </EncryptedData>
+</encryption>
+"""
+
+
+def test_decryptor():
+    from mock import MagicMock, patch, call, sentinel
+    from DeDRM_plugin.ineptepub import Decryptor
+    import DeDRM_plugin.ineptepub
+    AES = MagicMock()
+    RSA = MagicMock()
+    DeDRM_plugin.ineptepub.AES = AES
+    DeDRM_plugin.ineptepub.RSA = RSA
+    key_marker = b'key'
+    d = Decryptor(key_marker, ENCRYPTION_XML)
+    AES.assert_called_with(key_marker)
+    assert len(d._encrypted) == 2
+    assert six.b('path1') in d._encrypted
+    assert six.b('path2') in d._encrypted
+
+    encrypted_marker = b'encrypted'
+    with patch('zlib.decompressobj') as zlib:
+        # decompress return values untouched
+        # flush append empty string
+        dc = MagicMock()
+        dc.decompress.side_effect = lambda a: a
+        dc.flush.return_value = b''
+        # install mock
+        zlib.return_value = dc
+        # perform decompress
+        value = d.decompress(encrypted_marker)
+        zlib.assert_called_with(-15)
+        dc.decompress.has_calls(
+            call(encrypted_marker),
+            call(b'Z')
+        )
+        dc.flush.assert_called()
+        # check that remaining bytes are appended when available
+        assert value == encrypted_marker + b'Z'
+
+        dc.decompress.side_effect = lambda a: a if a != b'Z' else b''
+        value = d.decompress(encrypted_marker)
+        zlib.assert_called_with(-15)
+        dc.decompress.has_calls(
+            call(encrypted_marker),
+            call(b'Z')
+        )
+        dc.flush.assert_called()
+        # in this case no reminaing byte is applied
+        assert value == encrypted_marker
+
+        with patch('DeDRM_plugin.ineptepub.Decryptor.decompress') as dd:
+            dd.return_value = sentinel.decompress
+            data_marker = b''.join(map(bytes, range(0, 30)))
+            # last value is used to cut value
+            # 3 -> last three bytes are cut
+            cut = 3
+            data_marker = data_marker + bytes([cut])
+            decrypted = d.decrypt('path1', data_marker)
+            # 16 first bytes are skipped to call decrypt
+            assert AES.decrypt.is_called_with(data_marker[16:])
+            # decrypted value is cut before decompress
+            assert dd.is_called_with(data_marker[16:-cut])
+            # returned value is decompress result
+            assert decrypted == sentinel.decompress
 
 
 def loadAES_RSA():
